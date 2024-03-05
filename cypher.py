c71da43f39112e7419dc42a793311c2fd6f50ff7
Aiadiabibiud <162177806+Aiadiabibiud@users.noreply.github.com>
2024-03-05 11:27:38 -0500

    Update cypher.py

--- cypher.py
+++ cypher.py
@@ -1,11 +1,20 @@
 #Aidan Lembcke
 #SN: 340930908
-def cypher(word, shift):
+def cypher(word):
     '''
-    This function takes an alphanumeric value and an integer value and shifts all of the alphabetical characters
-    by the integer provided
+    this encrypts the target string using Caesar encryption
+
+    arguments
+        target : string
+        value : integer
+
+    return
+        string : encrypted version of target
     '''
-    shift = int(shift)%26
+    word = word.split(', ')
+    shift = word[-1]
+    word = word[0]
+    shift = int(shift)%26 # This line turns the number string into an integer and also takes the remainder of a division by 26 to keep the provided value within the range of the alphabet. 
     word = word.lower()
     chs = ''
     for i in word:
